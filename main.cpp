f1ec2cec096187bf904abdaaf68d89b34ec88c5e
Andrew Ng <ngandrew1337@gmail.com>
2021-02-14 19:08:36 -0900

    main.cpp: Write initial main

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// main.cpp
+// Andrew Ng, Rose Peters
+// Created: 2021-02-14
+//
+// For CS 372 Spring 2021
+// Main for Battleship Project
+
+#include "game.h"
+
+
+int main()
+{
+    Game game;
+
+    game.inputShip("ship1");
+    game.switchPlayer();
+    // game.hitTarget();
+}
